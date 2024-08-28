c717d10a90fbba8154996beaff79ad04b851231e
MairaEsper <mairaesper24@gmail.com>
2024-08-28 12:40:38 -0400

    hola

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
 #include <iostream>
+using namespace std;
+int main(){
+    int l = 1;
+    int *P = &l;
+    int x = 0;
+
+    cout<<P<<endl;
+    cout<<P+1<<endl;
+    cout<<&x<<endl;
+
+    return 0;
+    
+}
